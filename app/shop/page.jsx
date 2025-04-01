b7ffd404d9bcdba469978303f634283f9137948b
AndrewHypster <andrewgrechukh123@gmail.com>
2025-04-01 23:10:42 +0300

    riri

--- app/shop/page.jsx
+++ app/shop/page.jsx
@@ -1,28 +1,10 @@
-'use client'
-import { useSearchParams } from "next/navigation"
-import './shop.css'
-import Card from "@/components/card/card"
-import shop from '@/shop-db.json'
-import OrderForm from "@/components/order-form/form"
+import { Suspense } from "react";
+import ShopPage from "./ShopPage";
 
-const Shop = () => {
-  const pageParams = useSearchParams()
-  const category = pageParams.get('category') //shop?search=текст
-  const products = shop[category]?.products
-
+export default function Page() {
   return (
-    <main className="shop">
-      <OrderForm />
-      <h1 className="shop-title">{category}</h1>
-      <ul className="shop-cards">
-        {products?.map(prod => 
-          <li key={prod.id}>
-            <Card firm={prod.firm} name={prod.name} price={prod.price} img={prod.img} />
-          </li>
-        )}
-      </ul>
-    </main>
-  )
+    <Suspense fallback={<div>Завантаження...</div>}>
+      <ShopPage />
+    </Suspense>
+  );
 }
-
-export default Shop
---
+++ app/shop/shopPage.jsx
@@ -0,0 +1,33 @@
+"use client";
+import { useSearchParams } from "next/navigation";
+import "./shop.css";
+import Card from "@/components/card/card";
+import shop from "@/shop-db.json";
+import OrderForm from "@/components/order-form/form";
+
+const ShopPage = () => {
+  const pageParams = useSearchParams();
+  const category = pageParams.get("category"); //shop?search=текст
+  const products = shop[category]?.products;
+
+  return (
+    <main className="shop">
+      <OrderForm />
+      <h1 className="shop-title">{category}</h1>
+      <ul className="shop-cards">
+        {products?.map((prod) => (
+          <li key={prod.id}>
+            <Card
+              firm={prod.firm}
+              name={prod.name}
+              price={prod.price}
+              img={prod.img}
+            />
+          </li>
+        ))}
+      </ul>
+    </main>
+  );
+};
+
+export default Shop;
